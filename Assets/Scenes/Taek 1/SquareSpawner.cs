34b72d784fea81b8a2d18c4ccaf7040542c1804a
BearBearQD <86070469+BearBearQD@users.noreply.github.com>
2025-09-10 13:32:18 -0400

    Break
    Alrigh tso for some reason its not ecactly working and I just dont know why, Im going to take a break since i have class soon and maybe take a look at this again latter

--- Assets/Scenes/Taek 1/SquareSpawner.cs
+++ Assets/Scenes/Taek 1/SquareSpawner.cs
@@ -3,8 +3,7 @@ using UnityEngine;
 
 public class SquareSpawner : MonoBehaviour
 {
-    public Vector3 size = new Vector3(10,10,0);
-    public Vector3 position = new Vector3(0, 0, 0);
+    public Vector2 position = new Vector2(0, 0);
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,10 +14,21 @@ public class SquareSpawner : MonoBehaviour
     void Update()
     {
         position = Input.mousePosition;
-        
-        if(Input.GetMouseButtonDown(0))
+        Vector2 topLeft = position + new Vector2(position.x - 2, position.y + 2);
+        Vector2 topRight = position + new Vector2(position.x + 2, position.y + 2);
+        Vector2 bottomLeft = position + new Vector2(position.x - 2, position.y - 2);
+        Vector2 bottomRight = position + new Vector2(position.x + 2, position.y - 2);
+        Debug.DrawLine(topLeft, topRight);
+        Debug.DrawLine(bottomLeft, bottomRight);
+        Debug.DrawLine(topLeft, bottomLeft);
+        Debug.DrawLine(topRight, bottomRight);
+
+        if (Input.GetMouseButtonDown(0))
         {
-            Gizmos.DrawCube(position, size);
+            Debug.DrawLine(topLeft, topRight);
+            Debug.DrawLine(bottomLeft, bottomRight);
+            Debug.DrawLine(topLeft, bottomLeft);
+            Debug.DrawLine(topRight, bottomRight);
         }
         
     }
