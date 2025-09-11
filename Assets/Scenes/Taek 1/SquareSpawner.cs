b39e09b5b7a59b5d69d7bea6737ca35d4533f7a6
BearBearQD <86070469+BearBearQD@users.noreply.github.com>
2025-09-10 21:06:22 -0400

    Finished Task 1
    Its pretty janky, I had to find an old forum post to figure that out but it works. Honestly the mouse delta function kinda sucks in unity so can you really blame me for this combacle

--- Assets/Scenes/Taek 1/SquareSpawner.cs
+++ Assets/Scenes/Taek 1/SquareSpawner.cs
@@ -1,4 +1,7 @@
 using System.Drawing;
+using System.IO.Compression;
+using System.Runtime.CompilerServices;
+using Unity.Collections;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -6,9 +9,11 @@ public class SquareSpawner : MonoBehaviour
 {
     public Vector2 position = new Vector2(0, 0);
     public GameObject Square;
-    public float scaleSpeed;
-    public float minScale = 0.2f;
-    public float maxScale = 5f;
+    public GameObject SquareMove;
+    public float scaleSpeed = 0.1f;
+    Vector2 mousescroll = new Vector2(0,0);
+    float x;
+    float y;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -23,6 +28,7 @@ public class SquareSpawner : MonoBehaviour
         position = Input.mousePosition;
         Ray ray = Camera.main.ScreenPointToRay(position);
         Vector3 spawnPos = ray.GetPoint(10f);
+       
 
 
         if (Input.GetMouseButtonDown(0))
@@ -33,13 +39,28 @@ public class SquareSpawner : MonoBehaviour
             Debug.Log(position.x);
             Debug.Log(position.y);  
         }
-
-        Square.transform.position = spawnPos;
-        float scroll = Input.GetAxis("Mouse ScrollWheel");
-        if (scroll != 0)
+        mousescroll = Input.mouseScrollDelta;
+        x += mousescroll.y;
+        y += mousescroll.y;
+        SquareMove.transform.localScale = new Vector2(x, y);
+        if (x > 4)
         {
+            x = 4;
         }
-        Square.transform.localScale = new Vector3(Mathf.Clamp(scroll, minScale, maxScale), Mathf.Clamp(scroll, minScale, maxScale), 2);
+        if (x < 0.1f)
+        {
+            x = 0.1f;
+        }
+        if(y < 4)
+        {
+            y = 4;
+        }
+        if(y < 0.1f)
+        {
+            y = 0.1f;
+        }
+
+        SquareMove.transform.position = spawnPos;
 
 
 
