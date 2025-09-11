8b2ac2d8a982307b5348d7089a962fa73cf1769c
BearBearQD <86070469+BearBearQD@users.noreply.github.com>
2025-09-10 20:36:57 -0400

    Second question Done
    Pretty easy when your using game objects instead of DRAW LINE

--- Assets/Scenes/Taek 1/SquareSpawner.cs
+++ Assets/Scenes/Taek 1/SquareSpawner.cs
@@ -6,6 +6,7 @@ public class SquareSpawner : MonoBehaviour
     public Vector2 position = new Vector2(0, 0);
     public GameObject Square;
 
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,17 +17,20 @@ public class SquareSpawner : MonoBehaviour
     void Update()
     {
         position = Input.mousePosition;
+        Ray ray = Camera.main.ScreenPointToRay(position);
+        Vector3 spawnPos = ray.GetPoint(10f);
+
 
         if (Input.GetMouseButtonDown(0))
         {
             Debug.Log("Clap");
-            Ray ray = Camera.main.ScreenPointToRay(position);
-            Vector3 spawnPos = ray.GetPoint(10f);
             Instantiate(Square, spawnPos, Quaternion.identity);
             
             Debug.Log(position.x);
             Debug.Log(position.y);  
         }
-        
+        Square.transform.position = spawnPos;
+
+
     }
 }
